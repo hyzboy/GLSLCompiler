512cca5b59d0abc31c0b652c06e2d2c5eacc2d33
hyzboy <hyzboy@gmail.com>
2020-09-03 16:23:07 +0800

    force use CoreProfile and Vulkan1.2

--- glsl2spv.cpp
+++ glsl2spv.cpp
@@ -405,8 +405,9 @@ extern "C"
 
         shaderStrings[0] = shader_source;
         shader.setStrings(shaderStrings, 1);
+        shader.setEnvClient(glslang::EShClientVulkan, glslang::EShTargetVulkan_1_2);
 
-        if (!shader.parse(&Resources, 100, false, messages))
+        if (!shader.parse(&Resources, 100, ECoreProfile, false, false, messages))
             return(new SPVData(shader.getInfoLog(),shader.getInfoDebugLog()));
 
         program.addShader(&shader);
